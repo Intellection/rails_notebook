cfcce16e9422e52c6484e16f58fcd2fdcd91f417
Nick Hoernle <nick@intellectionsoftware.com>
2016-01-18 10:44:23 +0200

    sync

--- lib/rails_notebook/renderers.rb
+++ lib/rails_notebook/renderers.rb
@@ -17,62 +17,38 @@ module RailsNotebook
         # to import the files "d3.min.js" and "custom.js"
         def self.somejavascript( obj )
             <<-HTML
-            <div id="hash-#{obj.object_id}"></div>
+            <svg width=650 height=680>
+                <!--<div id="hash-#{obj.object_id}"/>-->
+                <g transform="translate(20,20)" id="hash-#{obj.object_id}"/>
+            </svg>
             <script>
                 require([ "jquery" , "/kernelspecs/rails_notebook/d3.min.js", "/kernelspecs/rails_notebook/dagre-d3.js"], function($, d3, dagreD3) {
-                    console.log ( $ );  
-                    console.log ( d3 );
-                    console.log ( dagreD3 );
+                    // Create a new directed graph
+var g = new dagreD3.Digraph();
 
-                    // var g =  new graphlib.Graph().setGraph({}).setDefaultEdgeLabel(function() { return {}; });
+// Add nodes to the graph. The first argument is the node id. The second is
+// metadata about the node. In this case we're going to add labels to each of
+// our nodes.
+g.addNode("kspacey",    { label: "Kevin Spacey" });
+g.addNode("swilliams",  { label: "Saul Williams" });
+g.addNode("bpitt",      { label: "Brad Pitt" });
+g.addNode("hford",      { label: "Harrison Ford" });
+g.addNode("lwilson",    { label: "Luke Wilson" });
+g.addNode("kbacon",     { label: "Kevin Bacon" });
 
-                    // g.setNode(0,  { label: "TOP",       class: "type-TOP" });
-                    // g.setNode(1,  { label: "S",         class: "type-S" });
-                    // g.setNode(2,  { label: "NP",        class: "type-NP" });
-                    // g.setNode(3,  { label: "DT",        class: "type-DT" });
-                    // g.setNode(4,  { label: "This",      class: "type-TK" });
-                    // g.setNode(5,  { label: "VP",        class: "type-VP" });
-                    // g.setNode(6,  { label: "VBZ",       class: "type-VBZ" });
-                    // g.setNode(7,  { label: "is",        class: "type-TK" });
-                    // g.setNode(8,  { label: "NP",        class: "type-NP" });
-                    // g.setNode(9,  { label: "DT",        class: "type-DT" });
-                    // g.setNode(10, { label: "an",        class: "type-TK" });
-                    // g.setNode(11, { label: "NN",        class: "type-NN" });
-                    // g.setNode(12, { label: "example",   class: "type-TK" });
-                    // g.setNode(13, { label: ".",         class: "type-." });
-                    // g.setNode(14, { label: "sentence",  class: "type-TK" });
+// Add edges to the graph. The first argument is the edge id. Here we use null
+// to indicate that an arbitrary edge id can be assigned automatically. The
+// second argument is the source of the edge. The third argument is the target
+// of the edge. The last argument is the edge metadata.
+g.addEdge(null, "kspacey",   "swilliams", { label: "K-PAX" });
+g.addEdge(null, "swilliams", "kbacon",    { label: "These Vagabond Shoes" });
+g.addEdge(null, "bpitt",     "kbacon",    { label: "Sleepers" });
+g.addEdge(null, "hford",     "lwilson",   { label: "Anchorman 2" });
+g.addEdge(null, "lwilson",   "kbacon",    { label: "Telling Lies in America" });
 
-                    // // Set up edges, no special attributes.
-                    // g.setEdge(3, 4);
-                    // g.setEdge(2, 3);
-                    // g.setEdge(1, 2);
-                    // g.setEdge(6, 7);
-                    // g.setEdge(5, 6);
-                    // g.setEdge(9, 10);
-                    // g.setEdge(8, 9);
-                    // g.setEdge(11,12);
-                    // g.setEdge(8, 11);
-                    // g.setEdge(5, 8);
-                    // g.setEdge(1, 5);
-                    // g.setEdge(13,14);
-                    // g.setEdge(1, 13);
-                    // g.setEdge(0, 1);
+                //var html = "<svg width=650 height=680><g transform="translate(20,20)"/></svg>"
 
-
-                    // // Create the renderer
-                    // var render = new dagreD3.render();
-
-                    // // Set up an SVG group so that we can translate the final graph.
-                    // var svg = d3.select("svg"),
-                    //     svgGroup = svg.append("g");
-
-                    // // Run the renderer. This is what draws the final graph.
-                    // render(d3.select("svg g"), g);
-
-                    // // Center the graph
-                    // var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
-                    // svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
-                    // svg.attr("height", g.graph().height + 40);
+                    $("#hash-#{obj.object_id}").append( "html" )
                 });
             </script>
             HTML
@@ -133,8 +109,8 @@ module RailsNotebook
         case obj
         when Array
             #Ruby does the heavy lifting here
-        when Hash
-            Renderers.json_view( obj ) # render as a json object
+        # when Hash
+        #     Renderers.json_view( obj ) # render as a json object
         when ActionDispatch::Routing::RouteSet
             Renderers.routes_view( obj )
             Renderers.somejavascript( obj )
