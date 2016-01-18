904ca7c714ea607691cd52c0cf3806ce48f2f018
Nick Hoernle <nick@intellectionsoftware.com>
2016-01-18 18:02:58 +0200

    Display first level of tree successfully

--- lib/rails_notebook/renderers.rb
+++ lib/rails_notebook/renderers.rb
@@ -22,94 +22,64 @@ module RailsNotebook
             <script>
             require(["jquery", "/kernelspecs/rails_notebook/dagre-d3.js", "/kernelspecs/rails_notebook/d3.js"], function ($, dagreD3 , d3 ) {
             routeTree = #{MultiJson.dump( tree )};
-            console.log( routeTree )
-            // Create the input graph
-            var g = new dagreD3.graphlib.Graph()
-              .setGraph({})
-              .setDefaultEdgeLabel(function() { return {}; });
 
-            // Here we"re setting nodeclass, which is used by our custom drawNodes function
-            // below.
             var buildOutput = function myself( g, thisNode, nodeNumber, i ){
-                console.log( thisNode );
-
+                if ( nodeNumber == 0 ) {
+                    g.setNode( 0 , {label:thisNode.uriPattern , class:"type-TK" } );
+                }
                 for ( c in thisNode.childrenNodes ){
                     child = thisNode.childrenNodes[c];
                     child.nodeNumber = nodeNumber + i;
-                    g.setNode( child.nodeNumber , {label:thisNode.uriPattern , class:"type-TK" } );
+                    g.setNode( child.nodeNumber , {label:child.uriPattern , class:"type-TK" } );
                     g.setEdge( nodeNumber , child.nodeNumber );
                     i++;
                 }
-                for ( c in thisNode.childrenNodes ){
-                    child = thisNode.childrenNodes[c];
-                    i = myself( g , child, child.nodeNumber,  i);
-                }
-                return i;
-                // countChildren = 0;
-                // var i = 1;
                 // for ( c in thisNode.childrenNodes ){
-                //     child = thisNode.childrenNodes[c]
-                //     myself( g, child, nodeNumber + countChildren + i, nodeNumber );
-                //     countChildren += child.countChildrenNodes;
-                //     i += 1;
+                //     child = thisNode.childrenNodes[c];
+                //     i = myself( g , child, child.nodeNumber,  i);
                 // }
             }
             
-            thisNode = routeTree.headNode.childrenNodes[0];
-            buildOutput( g , thisNode, 0 , 0 )
+            var g = new dagreD3.graphlib.Graph()
+                            .setGraph({})
+                            .setDefaultEdgeLabel(function() { return {}; });
 
-            // g.setNode(0,  { label: "TOP",       class: "type-TOP" });
-            // g.setNode(1,  { label: "S",         class: "type-S" });
-            // g.setNode(2,  { label: "NP",        class: "type-NP" });
-            // g.setNode(3,  { label: "DT",        class: "type-DT" });
-            // g.setNode(4,  { label: "This",      class: "type-TK" });
-            // g.setNode(5,  { label: "VP",        class: "type-VP" });
-            // g.setNode(6,  { label: "VBZ",       class: "type-VBZ" });
-            // g.setNode(7,  { label: "is",        class: "type-TK" });
-            // g.setNode(8,  { label: "NP",        class: "type-NP" });
-            // g.setNode(9,  { label: "DT",        class: "type-DT" });
-            // g.setNode(10, { label: "an",        class: "type-TK" });
-            // g.setNode(11, { label: "NN",        class: "type-NN" });
-            // g.setNode(12, { label: "example",   class: "type-TK" });
-            // g.setNode(13, { label: ".",         class: "type-." });
-            // g.setNode(14, { label: "sentence",  class: "type-TK" });
+
+            thisNode = routeTree.headNode;
+            buildOutput( g , thisNode, 0 , 1 )
 
             g.nodes().forEach(function(v) {
               var node = g.node(v);
               // Round the corners of the nodes
               node.rx = node.ry = 5;
             });
+            
+            // Set up an SVG group so that we can translate the final graph.
+            var svg = d3.select("svg"),
+                inner = svg.select("g");
 
-            // Set up edges, no special attributes.
-            // g.setEdge(3, 4);
-            // g.setEdge(2, 3);
-            // g.setEdge(1, 2);
-            // g.setEdge(6, 7);
-            // g.setEdge(5, 6);
-            // g.setEdge(9, 10);
-            // g.setEdge(8, 9);
-            // g.setEdge(11,12);
-            // g.setEdge(8, 11);
-            // g.setEdge(5, 8);
-            // g.setEdge(1, 5);
-            // g.setEdge(13,14);
-            // g.setEdge(1, 13);
-            // g.setEdge(0, 1)
+            // Set up zoom support
+            var zoom = d3.behavior.zoom().on("zoom", function() {
+                  inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                              "scale(" + d3.event.scale + ")");
+                });
+            
+            svg.call(zoom);
 
             // Create the renderer
             var render = new dagreD3.render();
 
-            // Set up an SVG group so that we can translate the final graph.
-            var svg = d3.select("svg"),
-                svgGroup = svg.append("g");
-
             // Run the renderer. This is what draws the final graph.
-            render(d3.select("svg g"), g);
+            render(inner, g);
+
 
             // Center the graph
-            var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
-            svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
-            svg.attr("height", g.graph().height + 40);
+            var initialScale = 0.75;
+            zoom
+              .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+              .scale(initialScale)
+              .event(svg);
+            svg.attr('height', g.graph().height * initialScale + 40);
 
 
             });
@@ -140,14 +110,13 @@ module RailsNotebook
     IRuby::Display::Registry.format("text/html") do |route_set|
         all_routes = route_set.routes.to_a
         all_routes.reject! { |route| route.verb.nil? || route.path.spec.to_s == '/assets' }
-        routeTree = RouteTree.new( RouteNode.new( "/" , "GET", "Default" , "index" ) )
+        routeTree = RouteTree.new( RouteNode.new( "/" , nil, nil, nil ) )
         all_routes.group_by { |route| route.defaults[:controller] }.each_value do |group|
             group.each do |r|
                 routeNode = RouteNode.new( (r.path.spec.to_s.sub '(.:format)','') , r.verb.inspect.gsub(/^.{2}|.{2}$/, "") , r.name.to_s , r.defaults[:action].to_s )
                 routeTree.insertNode( routeNode )
             end
         end
-        routeTree.getRoot.countChildrenNodes(0)
         Renderers.render_routes( routeTree )
     end
 
--- lib/rails_notebook/route.rb
+++ lib/rails_notebook/route.rb
@@ -8,48 +8,39 @@ class RouteNode
 		@countChildrenNodes = 0
 		@childrenNodes = []
 	end
-
 	def updateParams( _verb , _controller , _action )
 		@verbs << _verb unless _verb.nil?
 		@controller = _controller
 		@actions << _action unless _action.nil?
 	end
-
 	def verbs
   		@verbs
 	end
-
 	def verb
   		@verbs.first
 	end
-
 	def controller
 		@controller
 	end
-
 	def actions
 		@actions
 	end
-
 	def action
 		@actions.first
 	end
-
 	def uriPattern
   		@uriPattern
 	end
-
 	def getChildren
   		@childrenNodes
 	end
-
 	def getCountChildrenNodes
 		@countChildrenNodes
 	end
 
 	def hasRoute( uri_mapping )
 		@childrenNodes.each do |childNode|
-			if childNode == uri_mapping 
+			if childNode.uriPattern == uri_mapping 
 				return childNode
 			end
 		end
@@ -59,10 +50,6 @@ class RouteNode
 	def insertChild( childNode )
 		@childrenNodes << childNode
 	end
-
-	def ==( uri_mapping )
-    	self.uriPattern == uri_mapping
-  	end
 
   	def countChildrenNodes( count )
     	@childrenNodes.each do |child|
@@ -96,26 +83,24 @@ class RouteTree
 	def insertNode( _node )
 		#Get route mapping and iteratively either walk the tree or insert a node at the correct position
 		uri_mapping = ""
-		thisNode = @headNode
+		thisNode = @headNode #always insert from the headNode
+
 		_node.uriPattern.split("/").each do |uri|
             uri_mapping += uri + "/"
-            if uri_mapping[0...-1] == _node.uriPattern 
-            	# If we are at the correct node
-            	if thisNode.hasRoute( uri_mapping )
-            		# Update the parameters to include details
+            if uri_mapping[0...-1] == _node.uriPattern || uri_mapping == _node.uriPattern # We are at node to insert or update
+            	if thisNode.uriPattern == uri_mapping # Update the parameters to include details
+            		thisNode.updateParams( _node.verb , _node.controller , _node.action ) 
+            	elsif thisNode.hasRoute( uri_mapping )
             		thisNode.hasRoute( uri_mapping ).updateParams( _node.verb , _node.controller , _node.action )
-            		return 
             	else 
-            		# Insert a new child node
             		child = RouteNode.new( uri_mapping , _node.verb , _node.controller , _node.action )
             		thisNode.insertChild( child )
-            		return
             	end
-            elsif thisNode.hasRoute( uri_mapping )
-            	# node exists so walk the tree
+            elsif thisNode.hasRoute( uri_mapping ) # node exists so walk the tree
             	thisNode = thisNode.hasRoute( uri_mapping )
+            elsif thisNode.uriPattern == uri_mapping # already there
+            	thisNode.updateParams( _node.verb , _node.controller , _node.action )
             else
-            	# node doesn't exist so temporarily insert as placeholder
             	child = RouteNode.new( uri_mapping , nil, nil , nil )
             	thisNode.insertChild( child )
             	thisNode = child
