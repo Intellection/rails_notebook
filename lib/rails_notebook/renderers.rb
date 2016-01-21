2a8129b6d008cfcac3d4cf2177a9e3804b33e1ca
Tsu-Shiuan Lin <Tsu@Tsu-Shiuans-MacBook-Pro.local>
2016-01-21 10:17:51 +0200

    Schema first version, merging from master

--- lib/rails_notebook/renderers.rb
+++ lib/rails_notebook/renderers.rb
@@ -112,6 +112,83 @@ module RailsNotebook
             </script>
             HTML
         end
+
+        def self.render_schema( tables )
+            puts "Entering Javascript!"
+        <<-HTML
+            <svg width=960 height=600><g/></svg>
+            <!--<script src="/kernelspecs/rails_notebook/renderSchema.js"></script>-->
+            <script>
+            require(["jquery", "/kernelspecs/rails_notebook/dagre-d3.js", "/kernelspecs/rails_notebook/d3.js"], 
+                    function ( $ , dagreD3 , d3 ) {
+                var tablesVar = #{MultiJson.dump( tables )};
+                console.log("Processed requires successfully!")
+                console.log(tablesVar[0].table_name)
+
+                // Lets start by creating something simple and add lables to them
+                var g = new dagreD3.graphlib.Graph().setGraph({});
+
+                //Create rectangles for each of the tables
+                var tablesVarLangth = tablesVar.length;
+                console.log(tablesVarLangth)
+                for (var i=tablesVarLangth-1; i >= 0; i--)
+                {
+                    var buildHtmlOutput = "<div style = 'text-align: left;'><span style='font-size:32px;color:#00BFFF'><u>" + tablesVar[i].table_name + "</u></span><br>";
+                    tablesVar[i].columns.forEach(function(column_name){
+                        buildHtmlOutput = buildHtmlOutput + "<br>" + "<span style='font-size:24px;color:black'>" + column_name + "</span>";
+                    });
+                    buildHtmlOutput += "</div>";
+                    g.setNode(tablesVar[i].table_name, { shape: "rect", labelType: "html", label: buildHtmlOutput});
+                } // End of loop i, but not end of scope for i
+
+                // Now we want to populate the rectangles with column information
+                // TODO
+
+                // Now we want to make the arrows from the arrowsTo array
+                for (var i=0; i < tablesVarLangth; i++)
+                {
+                    if (tablesVar[i].arrowsTo.length != 0)
+                    {
+                        console.log(tablesVar[i].arrowsTo)
+                        tablesVar[i].arrowsTo.forEach(function(arrow) {
+                            g.setEdge(tablesVar[i].table_name, tablesVar[i].arrowsTo[0], {
+                                arrowhead: "normal",
+                                label: "belongs to"
+                          });
+                        });// For each arrow in table
+                    } // If there are arrows
+                } // End of loop i, but not end of scope for i
+                
+
+                var svg = d3.select("svg"),
+                    inner = svg.select("g");
+
+                // Set up zoom support
+                var zoom = d3.behavior.zoom().on("zoom", function() {
+                      inner.attr("transform", "translate(" + d3.event.translate + ")" +
+                                                  "scale(" + d3.event.scale + ")");
+                    });
+                svg.call(zoom);
+
+                // Create the renderer
+                var render = new dagreD3.render();
+
+                // Run the renderer. This is what draws the final graph.
+                render(inner, g);
+
+                // Center the graph
+                var initialScale = 1.5;
+                zoom
+                  .translate([(svg.attr("width") - g.graph().width * initialScale) / 2, 20])
+                  .scale(initialScale)
+                  .event(svg);
+                svg.attr('height', g.graph().height * initialScale + 40);
+
+            });   
+            </script>
+            HTML
+        end #render_schema()
+
     end
 
     IRuby::Display::Registry.type { Hash }
@@ -152,6 +229,52 @@ module RailsNotebook
             end
         end
         Renderers.render_routes( routeTree )
+    end
+
+    IRuby::Display::Registry.type { ActiveRecord::Base.connection.tables }
+    IRuby::Display::Registry.format("text/html") do |obj|
+        tables = [] # List of Table objects
+        tableNames = [] # List of table names
+        foreignKeys = []# foreignKeys - in format: table_name,column_name
+
+        obj.each do |table_name|
+            tableNames.push(table_name.singularize.foreign_key)
+        end # Populates table names
+
+        puts tableNames.to_s # Printing table names
+
+        obj.each do |table_name|
+            puts "\n" + table_name
+            columnsTemp = []
+            arrowsTo = []
+            ActiveRecord::Base.connection.columns(table_name).each do |c|
+                columnTemp = ""
+                if (tableNames.include? c.name)
+                    #puts "* " + c.name + ": " + c.type.to_s + " " + c.limit.to_s + " FOREIGN KEY"
+                    foreignKeys.push(table_name + "," + c.name)
+                    arrowsTo.push(c.name.humanize.pluralize.downcase)
+                    columnTemp += "* " + c.type.to_s.ljust(9) + " : " + c.name
+                else 
+                    #puts "- " + c.name + ": " + c.type.to_s + " " + c.limit.to_s
+                    columnTemp += "- " + c.type.to_s.ljust(9) + " : " + c.name
+                end
+                columnsTemp.push(columnTemp)
+            end # end iterating through columns in tables
+            tables.push(Table.new(table_name , columnsTemp , arrowsTo ))
+        end # end iterating through tables
+        puts "\n"
+        puts "\n"
+
+        puts "Foriegn keys table_name, column : " + foreignKeys.to_s
+        puts "\n"
+        puts "\n"
+
+        tables.each do |table|
+            table.printTable
+        end
+        # Lets first draw some rectangles for the table names
+        Renderers.render_schema( tables ) # Parses the array of tables to Javascript for rendering
+
     end
 
 end
